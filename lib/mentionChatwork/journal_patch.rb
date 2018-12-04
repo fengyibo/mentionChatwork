43305ba492102cb84a4ec3e4fff0216c3bb4122a
kariya <kariya@switch.am>
2018-12-04 15:10:43 +0900

    メンション機能の追加

--- lib/mentionChatwork/journal_patch.rb
+++ lib/mentionChatwork/journal_patch.rb
@@ -78,109 +78,7 @@ module RedmineMentions
             CGI.escape result
           end
       
-          private
-          def escape(msg)
-            msg.to_s
-          end
-      
-          def object_url(obj)
-            if Setting.host_name.to_s =~ /\A(https?\:\/\/)?(.+?)(\:(\d+))?(\/.+)?\z/i
-              host, port, prefix = $2, $4, $5
-              Rails.application.routes.url_for(obj.event_url({
-                   :host => host,
-                   :protocol => Setting.protocol,
-                   :port => port,
-                   :script_name => prefix
-               }))
-            else
-              Rails.application.routes.url_for(obj.event_url({
-                   :host => Setting.host_name,
-                   :protocol => Setting.protocol
-               }))
-            end
-          end
-      
-          def check_disabled(proj)
-            return nil if proj.blank?
-      
-            cf = ProjectCustomField.find_by_name("ChatWork Disabled")
-            state = proj.custom_value_for(cf).value rescue nil
-      
-            if state == nil
-              return false
-            end
-      
-            if state == '0'
-              return false
-            end
-      
-            true
-          end
-      
-          def room_for_project(proj)
-            return nil if proj.blank?
-      
-            cf = ProjectCustomField.find_by_name("ChatWork")
-      
-            val = [
-                Setting.plugin_mentionChatwork["room"],
-                (proj.custom_value_for(cf).value rescue nil),
-            ].find { |v| v.present? }
-      
-            rid = val.match(/#!rid\d+/)
-      
-            rid[0][5..val.length]
-          end
-      
-          def detail_to_field(detail)
-            if detail.property == "cf"
-              key = CustomField.find(detail.prop_key).name rescue nil
-              title = key
-            elsif detail.property == "attachment"
-              key = "attachment"
-              title = I18n.t :label_attachment
-            else
-              key = detail.prop_key.to_s.sub("_id", "")
-              title = I18n.t "field_#{key}"
-            end
-      
-            value = escape detail.value.to_s
-      
-            case key
-              when "tracker"
-                tracker = Tracker.find(detail.value) rescue nil
-                value = escape tracker.to_s
-              when "project"
-                project = Project.find(detail.value) rescue nil
-                value = escape project.to_s
-              when "status"
-                return ''
-                #status = IssueStatus.find(detail.value) rescue nil
-                #value = escape status.to_s
-              when "priority"
-                priority = IssuePriority.find(detail.value) rescue nil
-                value = escape priority.to_s
-              when "category"
-                category = IssueCategory.find(detail.value) rescue nil
-                value = escape category.to_s
-              when "assigned_to"
-                user = User.find(detail.value) rescue nil
-                value = escape user.to_s
-              when "fixed_version"
-                version = Version.find(detail.value) rescue nil
-                value = escape version.to_s
-              when "attachment"
-                attachment = Attachment.find(detail.prop_key) rescue nil
-                value = "<#{object_url attachment}|#{escape attachment.filename}>" if attachment
-              when "parent"
-                issue = Issue.find(detail.value) rescue nil
-                value = "<#{object_url issue}|#{escape issue}>" if issue
-            end
-      
-            value = "-" if value.empty?
-            result = "\n#{title}: #{value}"
-            result
-          end
+
       
 
 
@@ -189,6 +87,109 @@ module RedmineMentions
       end
     end
 
+    private
+    def escape(msg)
+      msg.to_s
+    end
+
+    def object_url(obj)
+      if Setting.host_name.to_s =~ /\A(https?\:\/\/)?(.+?)(\:(\d+))?(\/.+)?\z/i
+        host, port, prefix = $2, $4, $5
+        Rails.application.routes.url_for(obj.event_url({
+             :host => host,
+             :protocol => Setting.protocol,
+             :port => port,
+             :script_name => prefix
+         }))
+      else
+        Rails.application.routes.url_for(obj.event_url({
+             :host => Setting.host_name,
+             :protocol => Setting.protocol
+         }))
+      end
+    end
+
+    def check_disabled(proj)
+      return nil if proj.blank?
+
+      cf = ProjectCustomField.find_by_name("ChatWork Disabled")
+      state = proj.custom_value_for(cf).value rescue nil
+
+      if state == nil
+        return false
+      end
+
+      if state == '0'
+        return false
+      end
+
+      true
+    end
+
+    def room_for_project(proj)
+      return nil if proj.blank?
+
+      cf = ProjectCustomField.find_by_name("ChatWork")
+
+      val = [
+          Setting.plugin_mentionChatwork["room"],
+          (proj.custom_value_for(cf).value rescue nil),
+      ].find { |v| v.present? }
+
+      rid = val.match(/#!rid\d+/)
+
+      rid[0][5..val.length]
+    end
+
+    def detail_to_field(detail)
+      if detail.property == "cf"
+        key = CustomField.find(detail.prop_key).name rescue nil
+        title = key
+      elsif detail.property == "attachment"
+        key = "attachment"
+        title = I18n.t :label_attachment
+      else
+        key = detail.prop_key.to_s.sub("_id", "")
+        title = I18n.t "field_#{key}"
+      end
+
+      value = escape detail.value.to_s
+
+      case key
+        when "tracker"
+          tracker = Tracker.find(detail.value) rescue nil
+          value = escape tracker.to_s
+        when "project"
+          project = Project.find(detail.value) rescue nil
+          value = escape project.to_s
+        when "status"
+          return ''
+          #status = IssueStatus.find(detail.value) rescue nil
+          #value = escape status.to_s
+        when "priority"
+          priority = IssuePriority.find(detail.value) rescue nil
+          value = escape priority.to_s
+        when "category"
+          category = IssueCategory.find(detail.value) rescue nil
+          value = escape category.to_s
+        when "assigned_to"
+          user = User.find(detail.value) rescue nil
+          value = escape user.to_s
+        when "fixed_version"
+          version = Version.find(detail.value) rescue nil
+          value = escape version.to_s
+        when "attachment"
+          attachment = Attachment.find(detail.prop_key) rescue nil
+          value = "<#{object_url attachment}|#{escape attachment.filename}>" if attachment
+        when "parent"
+          issue = Issue.find(detail.value) rescue nil
+          value = "<#{object_url issue}|#{escape issue}>" if issue
+      end
+
+      value = "-" if value.empty?
+      result = "\n#{title}: #{value}"
+      result
+    end
 
   end
 end
